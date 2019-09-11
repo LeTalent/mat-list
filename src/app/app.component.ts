4b1e8f8c3309a956535680dce2eb4ee8a4aaace7
Maurice <MauriceNgassa.Happi@antrago.de>
2019-09-11 09:52:52 +0200

    mat-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
 
+export interface File {
+  name: string;
+  updated: Date;
+}
+
+export interface Message {
+  from: string;
+  subject: string;
+  content: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'LayoutList';
+  links: string[] = [ 'htpp://www.angular.com', 'http://www.deutche-bahn.de' ];
+  files: File[] = [
+    {
+      name: 'Photos',
+      updated: new Date('1/1/16'),
+    },
+    {
+      name: 'Recipes',
+      updated: new Date('1/17/16'),
+    },
+    {
+      name: 'Work',
+      updated: new Date('1/28/16'),
+    }
+  ];
+  messages: Message[] = [
+    {
+      from: 'jack',
+      subject: 'angular',
+      content: 'first content'
+    },
+    {
+      from: 'gatuso',
+      subject: 'git',
+      content: 'second content'
+    },
+    {
+      from: 'maldini',
+      subject: 'web',
+      content: 'third content is a subject that i just guest at the moment to make some fun'
+    }
+  ]
+
+  public showInfo(e: any) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatListModule, MatCardModule, MatIconModule } from '@angular/material';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, MatListModule, MatCardModule, MatIconModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
